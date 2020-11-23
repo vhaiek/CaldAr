8e0c3ce55d46849d9aa863b7030980f8b0f945de
vhaiek <haiek.vic@gmail.com>
2020-11-23 19:59:16 -0300

    server

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hi")
+const express = require("express");
+const app = express ();
+const PORT = process.env.PORT || 5000
+
+app.use("/api/companies",require("./controller/companies"));
+app.listen(PORT, () => console.log("Server started"));
