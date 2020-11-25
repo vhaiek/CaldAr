6e617a818d999536d8ece5784faa74803d0f7bb6
Lucas Dordolo <lucasd92@hotmail.com>
2020-11-24 22:48:31 -0300

    Added Boilers API

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-//app.use('/api/boilers',require('./controller/boilers'));
-app.use('/api/buildings', require('./controller/buildings'));
+app.use('/api/boilers',require('./controllers/boilers'));
+app.use('/api/buildings', require('./controllers/buildings'));
 app.use('/api/technicians',require('./controllers/technicians.js'));
-//app.use('/api/company',require('./controller/company'));
+//app.use('/api/company',require('./controllers/company'));
 
 app.listen(PORT, () => console.log('working OK'));
