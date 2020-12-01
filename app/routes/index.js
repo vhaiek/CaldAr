75c02ef436a1d4b97467ecdc985cdc26ac79d38d
leonel haiek <leonelhaiek@outlook.com>
2020-12-01 19:09:18 -0300

    created a new functions

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 // Set a path to each router
 router.use('/api/boilers',require('./boilers'));
 router.use('/api/technicians',require('./technicians'));
+
 router.use('/api/buildings',require('./buildings'));
 router.use('/api/companies', require('./companies'));
 
