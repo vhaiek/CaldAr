e9b86884dadd94b3ed7f5fe7bb0a4e9901abe9e4
Eloy Dordolo <eloydordolo87@gmail.com>
2020-12-07 07:33:53 -0300

    create .env file and comment out url environment variable

--- app/models/index.js
+++ app/models/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const db = {};
 db.mongoose = mongoose;
 db.url = 'mongodb+srv://eloy1234:eloy1234@caldar.fd9wx.mongodb.net/CaldAr?retryWrites=true&w=majority';
-
+//const URL = process.env.MONGO_DB_ATLAS
+//db.url = URL;
 db.boilers = require('./boilers.js')(mongoose);
 db.technicians = require('./technicians.js')(mongoose);
 db.buildings = require('./buildings.js')(mongoose);
