2ed45975ad6abf87edd68dbc891cc6c0dbb809be
leonel haiek <leonelhaiek@outlook.com>
2020-12-08 19:41:16 -0300

    validates

--- app/controllers/building.js
+++ app/controllers/building.js
@@ -5,121 +5,223 @@ const exp = {};
 
 // find all buildins
 exp.findAll = (req, res) => {
-  Building.find({})
+Building.find({})
     .then((data) => {
-      res.send(data);
+    res.send(data);
     })
     .catch((e) => {
-      res.status(500).send({
+    res.status(500).send({
         message: e.message || 'Some error ocurred while find all building ',
-      });
+    });
     });
 };
 
 // Find a single Building with an id
 exp.findOne = (req, res) => {
-  Building.findOne({ id: req.params.id })
+Building.findOne({ id: req.params.id })
     .then((data) => {
-      if (!data) {
+    if (!data) {
         return res.status(404).send({
-          message: 'Building with id ${req.params.id} was not found',
+        message: 'Building with id ${req.params.id} was not found',
         });
-      }
-      res.send(data);
+    }
+    res.send(data);
     })
     .catch((e) => {
-      res.status(500).send({
+    res.status(500).send({
         message:
-          e.message ||
-          'Some error ocurred while retrieving building with id ${req.params.id} ',
-      });
+        e.message ||
+        'Some error ocurred while retrieving building with id ${req.params.id} ',
+    });
     });
 };
 
 // Created a new building
 exp.create = (req, res) => {
-  if (
+if (
     !req.body.fullName ||
     !req.body.address ||
     !req.body.phone ||
     !req.body.boilers ||
     !req.body.id
-  ) {
+) {
     res.status(400).send({ message: 'Content can not be empty!!' });
     return;
-  }
-  const building = new Building({
+}
+};
+const building = new Building({
+id: req.body.id,
+fullName: req.body.fullName,
+address: req.body.address,
+phone: req.body.phone,
+boilers: req.body.boliers,
+});
+
+// Save Building in the databe
+building
+.save(building)
+.then((data) => {
+    res.send(data);
+})
+.catch((e) => {
+    res.status(500).send({
+    message:
+        e.message ||
+        'some error ocurrerd while creating building  ${req.params.id} ',
+    });
+});
+//Find a single Building with an id
+exp.findOne = (req, res) => {
+Building.findOne({ id: req.params.id })
+    .then((data) => {
+    if (!data) {
+        return res.status(404).send({
+        message: `Building with id ${req.params.id} was not found`,
+        });
+    }
+    res.send(data);
+    })
+    .catch((e) => {
+    res.status(500).send({
+        message:
+        e.message ||
+        `Some error ocurred while retrieving building with id ${req.params.id} `,
+    });
+    });
+};
+
+//Created a new building
+exp.create = (req, res) => {
+if (
+    !req.body.fullName ||
+    !req.body.address ||
+    !req.body.phone ||
+    !req.body.boilers ||
+    !req.body.id
+) {
+    res.status(400).send({ message: 'Content can not be empty!!' });
+    return;
+}
+const building = new Building({
     id: req.body.id,
     fullName: req.body.fullName,
     address: req.body.address,
     phone: req.body.phone,
     boilers: req.body.boliers,
-  });
-
-  // Save Building in the databe
-  building
-    .save(building)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((e) => {
-      res.status(500).send({
-        message:
-          e.message ||
-          'some error ocurrerd while creating building  ${req.params.id} ',
-      });
-    });
+});
+const error = building.validateSync();
+if (error != null) {
+    return res.status(400).send({ message: error.message });
+}
 };
+
+//Save Building in the databe
+building
+.save(building)
+.then((data) => {
+    res.send(data);
+})
+.catch((e) => {
+    res.status(500).send({
+    message:
+        e.message ||
+        `some error ocurrerd while creating building  ${req.params.id} `,
+    });
+});
 
 // Update Building
 
 exp.update = (req, res) => {
-  if (!req.body) {
+if (!req.body) {
     return res.status(400).sed({
-      message: 'Data to update can not be empty',
+    message: 'Data to update can not be empty',
     });
-  }
-  if (
+}
+if (
     !req.body.fullName ||
     !req.body.address ||
     !req.body.phone ||
     !req.body.boilers ||
     !req.body.id
-  ) {
+) {
     res.status(400).send({ message: 'Content can not be empty!!' });
     return;
-  }
-
-  const id = req.params.id;
-
-  Building.finOneAndUpdate({ id }, req.body, { useFindAndModify: flase })
-    .then((data) => {
-      if (!data) {
-        res.status(400).send({
-          message: `Cannot update building with id= ${req.params.id} `,
-        });
-      } else res.send({ message: 'Building was update successfully' });
-    })
-    .catch((e) => {
-      res.status(500).send({
-        message:
-          e.message ||
-          `some error ocurrerd while updating building  ${req.params.id} `,
-      });
-    });
+}
 };
+
+const id = req.params.id;
+
+Building.finOneAndUpdate({ id }, req.body, { useFindAndModify: flase })
+.then((data) => {
+    if (!data) {
+    res.status(400).send({
+        message: `Cannot update building with id= ${req.params.id} `,
+    });
+    } else res.send({ message: 'Building was update successfully' });
+})
+.catch((e) => {
+    res.status(500).send({
+    message:
+        e.message ||
+        `some error ocurrerd while updating building  ${req.params.id} `,
+    });
+});
 
 // Delete Building
 exp.delete = (req, res) => {
-  const id = req.params.id;
-  Building.findOneAndRemove({ id }, { useFindAndModify: false })
+const id = req.params.id;
+Building.findOneAndRemove({ id }, { useFindAndModify: false }).then((data) =>
+    res.send({ message: 'Buildin was delete successfully' })
+);
+if (!req.body) {
+    return res.status(400).sed({
+    message: 'Data to update can not be empty',
+    });
+}
+if (
+    !req.body.fullName ||
+    !req.body.address ||
+    !req.body.phone ||
+    !req.body.boilers ||
+    !req.body.id
+) {
+    res.status(400).send({ message: 'Content can not be empty!!' });
+    return;
+}
+const error = building.validateSync();
+if (error != null) {
+    return res.status(400).send({ message: error.message });
+}
+
+const id = req.params.id;
+Building.finOneAndUpdate({ id }, req.body, { useFindAndModify: flase })
+    .then((data) => {
+    if (!data) {
+        res.status(400).send({
+        message: `Cannot update building with id= ${req.params.id} `,
+        });
+    } else res.send({ message: 'Building was update successfully' });
+    })
+    .catch((e) => {
+    res.status(500).send({
+        message:
+        e.message ||
+        `some error ocurrerd while updating building  ${req.params.id} `,
+    });
+    });
+};
+
+//Delete Building
+exp.delete = (req, res) => {
+const id = req.params.id;
+Building.findOneAndRemove({ id }, { useFindAndModify: false })
     .then((data) => res.send({ message: 'Buildin was delete successfully' }))
     .catch((e) => {
-      res.status(500).send({
+    res.status(500).send({
         message:
-          e.message ||
-          `some error ocurrerd while delete building  ${req.params.id} `,
-      });
+        e.message ||
+        `some error ocurrerd while delete building  ${req.params.id} `,
+    });
     });
 };
 
--- app/models/buildings.js
+++ app/models/buildings.js
@@ -1,16 +1,62 @@
 module.exports = (mongoose) => {
-  const Building = mongoose.model(
+const Building = mongoose.model(
     'building',
     mongoose.Schema(
-      {
+    {
         id_building: String,
-        address: String,
-        boilers: [],
-        fullname: String,
-        phone: String,
-      },
-      { timestamps: true }
+        address: {
+        type: String,
+        validate: {
+            validator: function (v) {
+            return /^([a-z0-9]{2,}[\s]+)+([0-9]+)$/i.test(v);
+            },
+            message:
+            'Address must have at least 5 characters with numbers and letters',
+        },
+        },
+        boiler: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'boiler',
+        },
+        ],
+        capabilities: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'boilerType',
+        },
+        ],
+        hour_rate: {
+        type: Number,
+        min: [0, 'Hour rate must be positive'],
+        },
+        daily_capacity: {
+        type: Number,
+        min: [0, 'Daily capacity must be positive'],
+        },
+        fullname: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (v) {
+            return /^([a-z]{2,}[\s]+)+([a-z]{2,})$/i.test(v);
+            },
+            message: 'Name must have at least 6 characters. Ex: John Doe',
+        },
+        },
+        phone: {
+        type: String,
+        validate: {
+            validator: function (v) {
+            return /^[0-9]{8,}$/.test(v);
+            },
+            message:
+            'Phone Number must have at least 8 digits without spaces or special characters',
+        },
+        },
+    },
+    { timestamps: true }
     )
-  );
-  return Building;
+);
+return Building;
 };
