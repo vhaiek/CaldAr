c358751d912e2e408ef55bfaa6d29156824ab9aa
leonel haiek <leonelhaiek@outlook.com>
2020-12-08 22:14:58 -0300

    validationss

--- app/controllers/building.js
+++ app/controllers/building.js
@@ -36,60 +36,6 @@ Building.findOne({ id: req.params.id })
     });
 };
 
-// Created a new building
-exp.create = (req, res) => {
-if (
-    !req.body.fullName ||
-    !req.body.address ||
-    !req.body.phone ||
-    !req.body.boilers ||
-    !req.body.id
-) {
-    res.status(400).send({ message: 'Content can not be empty!!' });
-    return;
-}
-};
-const building = new Building({
-id: req.body.id,
-fullName: req.body.fullName,
-address: req.body.address,
-phone: req.body.phone,
-boilers: req.body.boliers,
-});
-
-// Save Building in the databe
-building
-.save(building)
-.then((data) => {
-    res.send(data);
-})
-.catch((e) => {
-    res.status(500).send({
-    message:
-        e.message ||
-        'some error ocurrerd while creating building  ${req.params.id} ',
-    });
-});
-//Find a single Building with an id
-exp.findOne = (req, res) => {
-Building.findOne({ id: req.params.id })
-    .then((data) => {
-    if (!data) {
-        return res.status(404).send({
-        message: `Building with id ${req.params.id} was not found`,
-        });
-    }
-    res.send(data);
-    })
-    .catch((e) => {
-    res.status(500).send({
-        message:
-        e.message ||
-        `Some error ocurred while retrieving building with id ${req.params.id} `,
-    });
-    });
-};
-
 //Created a new building
 exp.create = (req, res) => {
 if (
@@ -166,50 +112,6 @@ Building.finOneAndUpdate({ id }, req.body, { useFindAndModify: flase })
         `some error ocurrerd while updating building  ${req.params.id} `,
     });
 });
-
-// Delete Building
-exp.delete = (req, res) => {
-const id = req.params.id;
-Building.findOneAndRemove({ id }, { useFindAndModify: false }).then((data) =>
-    res.send({ message: 'Buildin was delete successfully' })
-);
-if (!req.body) {
-    return res.status(400).sed({
-    message: 'Data to update can not be empty',
-    });
-}
-if (
-    !req.body.fullName ||
-    !req.body.address ||
-    !req.body.phone ||
-    !req.body.boilers ||
-    !req.body.id
-) {
-    res.status(400).send({ message: 'Content can not be empty!!' });
-    return;
-}
-const error = building.validateSync();
-if (error != null) {
-    return res.status(400).send({ message: error.message });
-}
-
-const id = req.params.id;
-Building.finOneAndUpdate({ id }, req.body, { useFindAndModify: flase })
-    .then((data) => {
-    if (!data) {
-        res.status(400).send({
-        message: `Cannot update building with id= ${req.params.id} `,
-        });
-    } else res.send({ message: 'Building was update successfully' });
-    })
-    .catch((e) => {
-    res.status(500).send({
-        message:
-        e.message ||
-        `some error ocurrerd while updating building  ${req.params.id} `,
-    });
-    });
-};
 
 //Delete Building
 exp.delete = (req, res) => {
